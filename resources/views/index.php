970acb7f0cca6d14fbb2a8d86f2a1e2ec4bf3d02
Marijan3062 <brailo.marijan@gmail.com>
2019-06-07 16:15:41 +0200

    added some styling, functioning version for presentation

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,17 +1,23 @@
 <!DOCTYPE html>
-<html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
-    </head>
-    <body>
-   <?= \App\Http\Services\YoutubeService::video_list();?>
-      <?= $form ?>
-        
-        <script src="" async defer></script>
-    </body>
+<html class="no-js">
+<!--<![endif]-->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="/css/list.css">
+</head>
+
+<body>
+    <div class="samuel">
+        <?= \App\Http\Services\YoutubeService::video_list(); ?>
+        <?= $form ?>
+    </div>
+    <script src="" async defer></script>
+</body>
+
 </html>
--- resources/views/jukebox.php
+++ resources/views/jukebox.php
@@ -1,29 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Youtube Jukebox</title>
-    <link rel="stylesheet" href="css/list.css">
-</head>
-<body>
-    <?php if($display !== null) : ?>
+<?php if ($display !== null) : ?>
     <h1>Video display:</h1>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/<?=  $display->code ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    <div class="sammy"><iframe width="560" height="315" src="https://www.youtube.com/embed/<?= $display->code ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <?php endif; ?>
     <div class='list'>
         <h1>Playlist</h1>
         <ul>
-            <?php foreach($videos_list as $video) : ?>
-            <li><a href="/index?id=<?= $video->id ?>"><?= $video->name ?></a></li>
+            <?php foreach ($videos_list as $video) : ?>
+                <li><a href="/index?id=<?= $video->id ?>"><?= $video->name ?></a></li>
             <?php endforeach; ?>
 
-        
+
 
         </ul>
     </div>
-    
-</form>
-</body>
-</html>
+</div>
